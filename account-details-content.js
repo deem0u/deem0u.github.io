e88166e53275c646ce0f553d2a384f057ec3fc73
Daniel <103132500+deem0u@users.noreply.github.com>
2026-02-01 01:47:55 +1100

    Fix Edit Key Reset success page: remove footer gibberish, use hyphen for encoding

--- account-details-content.js
+++ account-details-content.js
@@ -23,15 +23,15 @@
       var viewLink = getViewLink(folder);
       var editLink = getEditLink(folder, editKey);
       return 'Below are details related to your account you should keep handy.\n\n' +
-        '• User Name: ' + (folder || '') + '\n' +
-        '• Edit Key: ' + (editKey || '') + '\n' +
-        '• Your Digital Contact Page URL: ' + viewLink + '\n' +
-        '• Your personalised edit link: ' + editLink + '\n\n' +
+        '\t• User Name: ' + (folder || '') + '\n' +
+        '\t• Edit Key: ' + (editKey || '') + '\n' +
+        '\t• Your Digital Contact Page URL: ' + viewLink + '\n' +
+        '\t• Your personalised edit link: ' + editLink + '\n\n' +
         'IMPORTANT: This personalised edit link and Edit Key is unique to you and gives you access to edit your Digital Contact Page. Please keep it private and do not share it with anyone else.\n\n' +
         'HOW TO UPDATE YOUR DIGITAL CONTACT PAGE\n' +
-        '1. Visit the Contact Editor (' + EDITOR_URL + ') and Sign In with your User Name and Edit Key or Click your personalised edit link above\n' +
-        '2. Make your changes in the Contact Editor\n' +
-        '3. Click "Save Changes" – Your updates will appear on your contact page within a few minutes\n\n' +
+        '\t1. Visit the Contact Editor (' + EDITOR_URL + ') and Sign In with your User Name and Edit Key or Click your personalised edit link above\n' +
+        '\t2. Make your changes in the Contact Editor\n' +
+        '\t3. Click "Save Changes" - Your updates will appear on your contact page within a few minutes\n\n' +
         'If at any point you wish to have your account/Digital Contact Page deleted, contact me at deem0u.github.io@gmail.com';
     },
     getBodyHtml: function(folder, editKey) {
@@ -46,7 +46,7 @@
         '<p class="success-heading">HOW TO UPDATE YOUR DIGITAL CONTACT PAGE</p>' +
         '<ol><li>Visit the Contact Editor (<a href="' + EDITOR_URL + '" target="_blank" rel="noopener">' + EDITOR_URL + '</a>) and Sign In with your User Name and Edit Key or Click your personalised edit link above</li>' +
         '<li>Make your changes in the Contact Editor</li>' +
-        '<li>Click "Save Changes" – Your updates will appear on your contact page within a few minutes</li></ol>' +
+        '<li>Click "Save Changes" - Your updates will appear on your contact page within a few minutes</li></ol>' +
         '<p>If at any point you wish to have your account/Digital Contact Page deleted, contact me at <a href="mailto:deem0u.github.io@gmail.com">deem0u.github.io@gmail.com</a></p>';
     }
   };
